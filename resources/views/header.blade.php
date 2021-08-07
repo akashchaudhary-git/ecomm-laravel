e3774d1b46de57c54ad987869dd49f94959e9b28
Akash Chaudhary <akash.chaudhary@live.in>
2021-08-07 12:51:35 +0530

    Header, Footer and Login page added

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -5,7 +5,7 @@
 --}}
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-      <a class="navbar-brand" href="#" style="color: #f34780; font-weight: 600; font-size: 24px; ">Reeta</a>
+      <a class="navbar-brand" href="#" style="color: #f34780; font-weight: 600; font-size: 24px; ">MyShop</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
